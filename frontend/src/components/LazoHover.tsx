9d8839ffdf3492f4fb275870974d928f24ccd9c1
lleis <lucas.mateo.leis@gmail.com>
2025-12-23 18:29:22 -0300

    avances menu 3

--- frontend/src/components/LazoHover.tsx
+++ frontend/src/components/LazoHover.tsx
@@ -5,38 +5,38 @@ interface Props {
 }
 
 export default function LazoHover({ active }: Props) {
-  return (
-    <motion.svg
-        viewBox="0 0 100 16"
+    return (
+        <motion.svg
+        viewBox="0 0 100 40"
         preserveAspectRatio="none"
-        className="
-            absolute left-0 bottom-0
-            w-full h-[1em]
-            pointer-events-none
-            z-0
-        "
-
-
+        className="absolute left-0 bottom-0 w-full h-[1.2em] pointer-events-none z-0"
         fill="none"
-    >
+        >
         <motion.path
             d="
-                M2 8
-                C18 2, 82 2, 98 8
-                C82 14, 18 14, 2 8
+                M12 18
+                C18 4, 42 6, 52 10
+                C74 16, 86 28, 76 34
+                C58 36, 26 32, 16 24
+                C8 20, 8 16, 12 18
             "
-            stroke="#ff6a3d"
+            stroke="#fd6647"
+            strokeOpacity="0.85"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
             initial={{ pathLength: 0, opacity: 0 }}
             animate={
-            active
-                ? { pathLength: 1, opacity: 1 }
-                : { pathLength: 0, opacity: 0 }
+                active
+                    ? { pathLength: 1, opacity: 0.55, rotate: [0, 0.5, 0] }
+                    : { pathLength: 0, opacity: 0 }
+                }
+                transition={{
+                duration: 0.6,
+                ease: "easeOut"
+                }
             }
-            transition={{ duration: 0.35, ease: "easeInOut" }}
-      />
-    </motion.svg>
-  );
+        />
+        </motion.svg>
+    );
 }
